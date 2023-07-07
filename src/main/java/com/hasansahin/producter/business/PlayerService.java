86257ff9a92673a871d45bd293364aa36918337c
hasanssahin <sahinhhasann@gmail.com>
2023-07-08 00:04:56 +0300

    Create services

---
+++ src/main/java/com/hasansahin/producter/business/PlayerService.java
@@ -0,0 +1,61 @@
+package com.hasansahin.producter.business;
+
+import com.hasansahin.producter.dto.converter.PlayerConverter;
+import com.hasansahin.producter.dto.reponse.PlayerCreateResponse;
+import com.hasansahin.producter.dto.request.PlayerCreateRequest;
+import com.hasansahin.producter.entity.Player;
+import com.hasansahin.producter.entity.enums.Positions;
+import com.hasansahin.producter.exception.CreatePlayerException;
+import com.hasansahin.producter.exception.FullTeamException;
+import com.hasansahin.producter.repository.PlayerRepository;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.Position;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+@Service
+public class PlayerService {
+	private final PlayerConverter playerConverter;
+	private final PlayerRepository playerRepository;
+
+	private final TeamService teamService;
+
+	public PlayerService(PlayerConverter playerConverter, PlayerRepository playerRepository, TeamService teamService) {
+		this.playerConverter = playerConverter;
+		this.playerRepository = playerRepository;
+		this.teamService = teamService;
+	}
+
+	public PlayerCreateResponse createPlayer(PlayerCreateRequest playerCreateRequest) {
+		if(playerRepository.count()>=10){
+			throw new FullTeamException("Takım doldu");
+		}
+		playerCreateRequest.setPosition(playerCreateRequest.getPosition().trim());
+		if(playerCreateRequest.getPosition()!=null && !playerCreateRequest.getPosition().isEmpty()){
+			try{
+				Positions.valueOf(playerCreateRequest.getPosition().toUpperCase(Locale.ENGLISH).replace(" ","_"));
+			}catch (IllegalArgumentException e){
+				throw new CreatePlayerException("Yanlış pozisyon girdisi");
+			}
+			Player player = playerConverter.convertPlayerCreateRequestToPlayer(playerCreateRequest);
+			player.setCreatedDate(new Date());
+			teamService.addPlayer(player);
+			playerRepository.save(player);
+			return playerConverter.convertPlayerToPlayerCreateResponse(player);
+		}else{
+			throw new CreatePlayerException("Pozisyon bilgisi boş");
+		}
+
+	}
+
+	public void deletePlayer(Long playerID) {
+		playerRepository.deleteById(playerID);
+	}
+
+	public List<PlayerCreateResponse> listAllPlayer() {
+		return playerConverter.convertPlayerToPlayerCreateResponseList(playerRepository.findAll());
+	}
+}
---
+++ src/main/java/com/hasansahin/producter/business/TeamService.java
@@ -0,0 +1,34 @@
+package com.hasansahin.producter.business;
+
+import com.hasansahin.producter.dto.converter.TeamConverter;
+import com.hasansahin.producter.dto.reponse.TeamResponse;
+import com.hasansahin.producter.entity.Player;
+import com.hasansahin.producter.entity.Team;
+import com.hasansahin.producter.repository.TeamRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeamService {
+	private final TeamRepository teamRepository;
+	private final TeamConverter teamConverter;
+
+	public TeamService(TeamRepository teamRepository, TeamConverter teamConverter) {
+		this.teamRepository = teamRepository;
+		this.teamConverter = teamConverter;
+	}
+
+	public Team addPlayer(Player player){
+		Team team=new Team();
+		player.setTeam(team);
+		team.addPlayer(player);
+		teamRepository.save(team);
+		return team;
+	}
+
+	public List<TeamResponse> listTeam(){
+		List<Team> teams=teamRepository.findAll();
+		return teamConverter.convertTeamToTeamResponseList(teams);
+	}
+}
