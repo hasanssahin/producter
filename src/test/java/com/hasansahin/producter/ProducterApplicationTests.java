f0880f0ffe5287940b189e00f0553f4ed8329d50
hasanssahin <sahinhhasann@gmail.com>
2023-07-08 00:05:38 +0300

    required

---
+++ src/test/java/com/hasansahin/producter/ProducterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasansahin.producter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
